7eebb4e1717d93f140cdb3ced02ca96eeb68ce08
deepdarshan21 <deepdarshan21@gmail.com>
2023-06-24 16:50:39 +0530

    check

--- pages/[userName].jsx
+++ pages/[userName].jsx
@@ -3,10 +3,13 @@ import { NextPage } from "next";
 import { Layout } from "../layouts/Layout";
 import { Meta } from "../layouts/Meta";
 import { Dashboard } from "../containers/Dashbord";
+import { useRouter } from "next/router";
 
 const Home = () => {
+    const router = useRouter();
+    const username = router.query.username;
     return (
-        <Layout meta={<Meta title="Deepdarshan | Github Visualizer" />} displayNav={false} displayFooter={false}>
+        <Layout meta={<Meta title={`${username} | Github Visualizer`} />} displayNav={false} displayFooter={false}>
             <Dashboard />
         </Layout>
     );
